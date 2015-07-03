2a856343cd4ce44a64f0f9f66a45591bbfcf5130
Simao Belchior <simaobelchior@gmail.com>
2015-07-03 12:27:11 +0100

    Fixed raster info and added one extra

--- examples.js
+++ examples.js
@@ -6,16 +6,29 @@ function getCartoCss(id, rules) {
 
 var examples = {
     'cros_2011_r': {
-        name: 'WorldBank CORS ST_ColorMap BLUERED',
-        desc: 'Worldbank example',
+        name: 'WorldBank CORS 2011 ST_ColorMap BLUERED',
+        desc: 'Worldbank example 2011',
         sql: [
             "SELECT ST_ColorMap(the_raster_webmercator,'bluered') the_raster_webmercator",
-            "FROM ne2_hr_lc_sr_w_dr"
+            "FROM cros_2011_r"
         ].join('\n'),
-        cartocss: getCartoCss('ne2_hr_lc_sr_w_dr', [
+        cartocss: getCartoCss('cros_2011_r', [
             'raster-opacity: 0.5;'
         ]),
         center: [20, 0],
         zoom: 2
-    }
+    },
+    'cros_2001_r': {
+        name: 'WorldBank CORS 2001 ST_ColorMap BLUERED',
+        desc: 'Worldbank example 2001',
+        sql: [
+            "SELECT ST_ColorMap(the_raster_webmercator,'bluered') the_raster_webmercator",
+            "FROM cros_2001_r"
+        ].join('\n'),
+        cartocss: getCartoCss('cros_2001_r', [
+            'raster-opacity: 0.5;'
+        ]),
+        center: [20, 0],
+        zoom: 2
+    },
 };
